fcbb29ebe6e4e403c260f7f419303202c9a058ff
reda777 <60011372+reda777@users.noreply.github.com>
2023-03-26 15:03:49 +0000

    Add header logic and some UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Header from './components/Header';
+class App extends Component{
+  render(){
+    return (
+    <div className='main'>
+      <div className='header'>
+        <Header />
+      </div>
+      <div className='experience'>
+        
+      </div>
+      <div className='education'>
+        
+      </div>
+      <div className='contact'>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className='skills'>
+        
+      </div>
     </div>
-  );
+    ); 
+  }
 }
-
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,92 @@
+import React, {Component} from 'react';
+import faceImage from '../images/image1.jpg';
+class Header extends Component{
+  constructor(){
+    super();
+    this.state={
+      renderPart: 'default',
+      personalImg: faceImage,
+      name: "First Name Last Name",
+      currentJob: "Current Job",
+    }
+    this.handleChange=this.handleChange.bind(this);
+    this.handleClick=this.handleClick.bind(this);
+    this.handleEnter=this.handleEnter.bind(this);
+    this.handleImageChange=this.handleImageChange.bind(this);
+  }
+  handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const id = target.id;
+    this.setState({
+      [id]: value,
+    });
+  }
+  renderMapObject(){
+    const renderMap = {
+      name: (
+        <form>
+          <label id='personalImg' onClick={this.handleClick}>
+            <input type="file" accept="image/jpeg, image/png, image/gif, image/svg+xml" onChange={this.handleImageChange} />
+            <img alt='Your Face' src={this.state.personalImg}/>
+          </label>
+          <input type='text' id='name' value={this.state.name} onChange={this.handleChange} onKeyDown={this.handleEnter}/>
+          <span id='currentJob' onClick={this.handleClick}>{this.state.currentJob}</span>
+        </form>
+      ),
+      currentJob: (
+        <form>
+          <label id='personalImg' onClick={this.handleClick}>
+            <input type="file" accept="image/jpeg, image/png, image/gif, image/svg+xml" onChange={this.handleImageChange} />
+            <img alt='Your Face' src={this.state.personalImg}/>
+          </label>
+          <span id='name' onClick={this.handleClick}>{this.state.name}</span>
+          <input type='text' id='currentJob' value={this.state.currentJob} onChange={this.handleChange} onKeyDown={this.handleEnter}/>
+        </form>
+      ),
+      default: (
+        <form>
+          <label id='personalImg' onClick={this.handleClick}>
+            <input type="file" accept="image/jpeg, image/png, image/gif, image/svg+xml" onChange={this.handleImageChange} />
+            <img alt='Your Face' src={this.state.personalImg}/>
+          </label>
+          <span id='name' onClick={this.handleClick}>{this.state.name}</span>
+          <span id='currentJob' onClick={this.handleClick}>{this.state.currentJob}</span>
+        </form>
+      )
+    };
+    return renderMap;
+  }
+  handleEnter(e){
+    if(e.key==="Enter"){
+      this.setState({renderPart: 0,})
+    }
+  }
+  handleClick(e){
+    this.setState({
+      renderPart: e.target.id,
+    });
+  }
+  handleImageChange(event){
+    const selectedFile = event.target.files[0];
+    if (selectedFile && selectedFile.type.startsWith('image/')) {
+      const imageUrl = URL.createObjectURL(selectedFile);
+      this.setState({
+        personalImg: imageUrl,
+      });
+    }else{
+      console.log("wrong file type");
+    }
+  }
+  render(){
+    const renderPart=this.state.renderPart;
+    let renderMap=this.renderMapObject();
+    let form=renderMap[renderPart] || renderMap.default;
+    return (
+      <div className='headerChild'>
+        {form}
+      </div>
+    ); 
+  }
+}
+export default Header;
